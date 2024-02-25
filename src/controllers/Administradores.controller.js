1c605203b0a33a7cea2977d2c2520b6e08145232
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-02-24 19:11:50 -0600

    Creacion de la funcion Generate token

--- src/controllers/Administradores.controller.js
+++ src/controllers/Administradores.controller.js
@@ -1,63 +1,61 @@
+// Importa la función para generar tokens JWT
+const { generateToken } = require('../helpers/jwt');
 import { GetConnection } from '../DataBase/contection/Conexion';
 import sql from 'mssql';
-
-// Controlador de administración para los stored procedures del administrador
-
-// Método para obtener todos los administradores
+// Controlador para manejar la obtención de todos los administradores
 export const GetAdministradores = async (req, res) => {
     try {
-        const pool = await GetConnection();
-        const result = await pool.request().query('EXEC SPObtenerAdministradores');
-        res.status(200).json(result.recordset);
+        const pool = await GetConnection(); // Establece la conexión a la base de datos
+        const result = await pool.request().query('EXEC SPObtenerAdministradores'); // Ejecuta el SP para obtener todos los administradores
+        res.status(200).json(result.recordset); // Devuelve los administradores en formato JSON
     } catch (error) {
         console.error(`Error al obtener los administradores: ${error}`);
         res.status(500).json({ msg: 'Error al obtener los administradores' });
     }
 };
-// Método para obtener un administrador por su Id
+
+// Controlador para obtener un administrador por su Id
 export const GetAdministradorPorId = async (req, res) => {
     const { id } = req.params;
     try {
-        const pool = await GetConnection();
-        const result = await pool.request().input('Id', sql.Int, id).query('EXEC SPObtenerAdministradorPorId @Id');
-
+        const pool = await GetConnection(); // Establece la conexión a la base de datos
+        const result = await pool.request().input('Id', sql.Int, id).query('EXEC SPObtenerAdministradorPorId @Id'); // Ejecuta el SP para obtener un administrador por su Id
         if (result.recordset.length > 0) {
-            res.status(200).json(result.recordset[0]);
+            res.status(200).json(result.recordset[0]); // Devuelve el administrador encontrado en formato JSON
         } else {
-            res.status(404).json({ msg: 'Administrador no encontrado' });
+            res.status(404).json({ msg: 'Administrador no encontrado' }); // Si no se encontró el administrador, devuelve un mensaje de error
         }
     } catch (error) {
         console.error(`Error al obtener el administrador: ${error}`);
         res.status(500).json({ msg: 'Error al obtener el administrador' });
     }
-}; 
-// Método para insertar un nuevo administrador
+};
+
+// Controlador para insertar un nuevo administrador
 export const PostAdministrador = async (req, res) => {
     const { nombre, apellido, idCargo, telefono, pass } = req.body;
-    // if (!nombre || !apellido || !idCargo || !telefono || !pass) {
-    //     return res.status(400).json({ msg: 'Todos los campos son requeridos' });
-    // }
     try {
-        const pool = await GetConnection();
+        const pool = await GetConnection(); // Establece la conexión a la base de datos
         await pool.request()
             .input('Nombre', sql.VarChar(50), nombre)
             .input('Apellido', sql.VarChar(50), apellido)
             .input('IdCargo', sql.TinyInt, idCargo)
             .input('Telefono', sql.VarChar(50), telefono)
             .input('Pass', sql.VarChar(200), pass)
-            .query('EXEC SPInsertarAdministrador @Nombre, @Apellido, @IdCargo, @Telefono, @Pass');
+            .query('EXEC SPInsertarAdministrador @Nombre, @Apellido, @IdCargo, @Telefono, @Pass'); // Ejecuta el SP para insertar un nuevo administrador
         res.status(201).json({ msg: 'Administrador creado correctamente' });
     } catch (error) {
         console.error(`Error al insertar el administrador: ${error}`);
         res.status(500).json({ msg: 'Error al insertar el administrador' });
     }
 };
-//put administrador o actualizar 
+
+// Controlador para actualizar un administrador
 export const PutAdministrador = async (req, res) => {
     const { id } = req.params;
     const { nombre, apellido, idCargo, telefono, pass } = req.body;
     try {
-        const pool = await GetConnection();
+        const pool = await GetConnection(); // Establece la conexión a la base de datos
         await pool.request()
             .input('Id', sql.Int, id)
             .input('Nombre', sql.VarChar(50), nombre)
@@ -65,7 +63,7 @@ export const PutAdministrador = async (req, res) => {
             .input('IdCargo', sql.TinyInt, idCargo)
             .input('Telefono', sql.VarChar(50), telefono)
             .input('Pass', sql.VarChar(200), pass)
-            .query('EXEC SPActualizarAdministrador @Id, @Nombre, @Apellido, @IdCargo, @Telefono, @Pass');
+            .query('EXEC SPActualizarAdministrador @Id, @Nombre, @Apellido, @IdCargo, @Telefono, @Pass'); // Ejecuta el SP para actualizar un administrador
         res.status(200).json({ msg: 'Administrador actualizado correctamente' });
     } catch (error) {
         console.error(`Error al actualizar el administrador: ${error}`);
@@ -73,13 +71,12 @@ export const PutAdministrador = async (req, res) => {
     }
 };
 
-
-// Método para eliminar un administrador por su Id
+// Controlador para eliminar un administrador por su Id
 export const DeleteAdministrador = async (req, res) => {
     const { id } = req.params;
     try {
-        const pool = await GetConnection();
-        await pool.request().input('Id', sql.int, id).query('EXEC SPEliminarAdministrador @Id');
+        const pool = await GetConnection(); // Establece la conexión a la base de datos
+        await pool.request().input('Id', sql.int, id).query('EXEC SPEliminarAdministrador @Id'); // Ejecuta el SP para eliminar un administrador por su Id
         res.status(200).json({ msg: 'Administrador eliminado correctamente' });
     } catch (error) {
         console.error(`Error al eliminar el administrador: ${error}`);
@@ -87,22 +84,27 @@ export const DeleteAdministrador = async (req, res) => {
     }
 };
 
-// Método para autenticación de administrador
+// Controlador para autenticación de administrador
 export const LoginAdministrador = async (req, res) => {
     const { nombre, pass } = req.body;
+
     try {
-        const pool = await GetConnection();
+        const pool = await GetConnection(); // Establece la conexión a la base de datos
         const result = await pool.request()
             .input('Nombre', sql.VarChar(50), nombre)
             .input('Pass', sql.VarChar(200), pass)
-            .query('EXEC SPLoginAdministrador @Nombre, @Pass');
-        if (result.recordset.length > 0 && result.recordset[0].Id) {
-            res.status(200).json({ msg: 'Inicio de sesión exitoso' });
+            .execute('SPLoginAdministrador'); // Ejecuta el SP para autenticar al administrador
+
+        const adminId = result.recordset[0].Id; // Obtiene el Id del administrador del resultado
+
+        if (adminId) {
+            const token = generateToken({ adminId }); // Genera un token JWT con el Id del administrador
+            res.status(200).json({ token }); // Devuelve el token como respuesta
         } else {
-            res.status(401).json({ msg: 'Credenciales inválidas' });
+            res.status(401).json({ msg: 'Credenciales incorrectas' }); // Si las credenciales son incorrectas, devuelve un mensaje de error
         }
     } catch (error) {
-        console.error(`Error al autenticar el administrador: ${error}`);
-        res.status(500).json({ msg: 'Error al autenticar el administrador' });
+        console.error(`Error al iniciar sesión: ${error}`);
+        res.status(500).json({ msg: 'Error al iniciar sesión' });
     }
 };
