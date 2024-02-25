afc0a3e6c6d45d65e2696fba244e751069514b76
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-02-24 19:13:12 -0600

    funcion para generar token

--- src/helpers/JWT.js
+++ src/helpers/JWT.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
+/* La función generateToken genera un token JWT utilizando la clave secreta secretKey y estableciendo una expiración de 1 hora. La función verifyToken verifica la validez del token utilizando la misma clave secreta. Si el token es válido, devuelve el contenido del token (payload); de lo contrario, lanza un error. */
 const secretKey = 'jeffrey'; // Cambia esto por una clave secreta segura
 const generateToken = (payload) => {
     return jwt.sign(payload, secretKey, { expiresIn: '1h' }); // El token expira en 1 hora
 };
 
-/* La función generateToken genera un token JWT utilizando la clave secreta secretKey y estableciendo una expiración de 1 hora. La función verifyToken verifica la validez del token utilizando la misma clave secreta. Si el token es válido, devuelve el contenido del token (payload); de lo contrario, lanza un error. */
 const verifyToken = (token) => {
     try {
         return jwt.verify(token, secretKey);
