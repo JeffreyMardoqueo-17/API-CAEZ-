0acd6bcab406964acbdb4ad05a4f34de5c45dfa7
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-05-20 10:14:09 -0600

    Validaciones de rol

--- src/routers/Role.routes.js
+++ src/routers/Role.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { GetRoles, GetRolPorId, PutRol, PostRol, DeleteRol } from '../controllers/Role';
 import { validateToken } from '../helpers/JWT';
+import {ValidateCreateRole, ValidateUpdateRole} from '../validators/Role.validator'
 
 const route = express.Router();
 
 route.get('/Role',validateToken, GetRoles);
 route.get('/Role/:id', validateToken, GetRolPorId);
-route.post('/Role', validateToken, PostRol);
-route.put('/Role/:id',validateToken, PutRol);
+route.post('/Role',ValidateCreateRole, validateToken, PostRol);
+route.put('/Role/:id',ValidateUpdateRole,validateToken, PutRol);
 route.delete('/Role/:id', validateToken, DeleteRol);
 
 export default route;
