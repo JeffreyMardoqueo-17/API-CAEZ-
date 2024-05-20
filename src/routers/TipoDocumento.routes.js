e00146dd7ba85092205e211da8ee6121064bd1e4
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-05-20 08:19:09 -0600

    Validacion de Grado

--- src/routers/TipoDocumento.routes.js
+++ src/routers/TipoDocumento.routes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { GetTiposDocumento, GetTipoDocumentoPorId, PostTipoDocumento, PutTipoDocumento, DeleteTipoDocumento, BuscarTipoDocumentoPorTexto } from '../controllers/TipoDocumento.controller';
-import { ValidatePostTipoDoc } from '../validators/TipoDocumento';
 import { validateToken } from '../helpers/JWT';
+import {ValidateCreateTipoDoc, ValidateUpdateTipoDoc} from '../validators/TipoDocumento.validator'
+
 const route = Router();
 
 // Rutas
 route.get('/TiposDocumento',validateToken, GetTiposDocumento);
 route.get('/TiposDocumento/:id', validateToken, GetTipoDocumentoPorId);
-route.post('/TiposDocumento',validateToken, PostTipoDocumento);
-route.put('/TiposDocumento/:id', validateToken, PutTipoDocumento);
+route.post('/TiposDocumento',ValidateCreateTipoDoc,validateToken, PostTipoDocumento);
+route.put('/TiposDocumento/:id',ValidateUpdateTipoDoc, validateToken, PutTipoDocumento);
 route.delete('/TiposDocumento/:id', validateToken, DeleteTipoDocumento);
 route.post('/TiposDocumento/Buscar/', validateToken, BuscarTipoDocumentoPorTexto) // Aquí está la ruta corregida
 
