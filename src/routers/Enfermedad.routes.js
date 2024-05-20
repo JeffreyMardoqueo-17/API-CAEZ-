f11a9735a3422e86566a5625d188aff894da90da
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-05-20 08:51:11 -0600

    validacion enfermedad

--- src/routers/Enfermedad.routes.js
+++ src/routers/Enfermedad.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { GetEnfermedades, GetEnfermedadPorId, PostEnfermedad, PutEnfermedad, DeleteEnfermedad,BuscarEnfermedadesPorTexto } from '../controllers/Enfermedad.controller';
 import { validateToken } from '../helpers/JWT';
+import {ValidateCreateEnfermedad, ValidateUpdateEnfermedad} from '../validators/Enfermedad.validator'
 
 const router = express.Router();
 
 router.get('/enfermedad',validateToken, GetEnfermedades);
 router.get('/enfermedad/:id', validateToken, GetEnfermedadPorId);
-router.post('/enfermedad', validateToken, PostEnfermedad);
-router.put('/enfermedad/:id',validateToken, PutEnfermedad);
+router.post('/enfermedad',ValidateCreateEnfermedad, validateToken, PostEnfermedad);
+router.put('/enfermedad/:id',ValidateUpdateEnfermedad,validateToken, PutEnfermedad);
 router.delete('/enfermedad/:id',validateToken, DeleteEnfermedad);
 router.post('/enfermedad/Buscar',validateToken, BuscarEnfermedadesPorTexto);
 
