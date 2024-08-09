31e2a52a2f1329b433cfd7868b67e2dcebeab803
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-08-08 18:09:55 -0600

    Parentezco listo

--- src/routers/Parentezco.routes.js
+++ src/routers/Parentezco.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { GetParentezcos, GetParentezcoPorId, PostParentezco, DeleteParentezco, PutParentezco, BuscarParentezcoPorTexto } from '../controllers/Parentezco.controller';
+import { } from '../controllers/Padrino.controller'
+import { GetParentezcos, DeleteParentezco, PutParentezco, PostParentezco, GetParentezcoPorId } from "../controllers/Parentezco.controller";
 import { validateToken } from '../helpers/JWT';
-import {ValidateCreateParentezco, ValidateUpdateParentezco} from '../validators/Parentezco.validator'
+import { ValidateCreateParentezco, ValidateUpdateParentezco } from '../validators/Parentezco.validator'
 
 const route = Router();
 
@@ -13,12 +14,11 @@ const route = Router();
 // route.put('/Parentezcos/:id',ValidateUpdateParentezco, validateToken, PutParentezco); 
 // route.post('/Parentezcos/Buscar', validateToken, BuscarParentezcoPorTexto);
 
-route.get('/Parentezcos',  GetParentezcos); 
+route.get('/Parentezcos', GetParentezcos);
 route.get('/Parentezcos/:id', GetParentezcoPorId);
-route.post('/Parentezcos',ValidateCreateParentezco,  PostParentezco);
-route.delete('/Parentezcos/:id', DeleteParentezco); 
-route.put('/Parentezcos/:id',ValidateUpdateParentezco,  PutParentezco); 
-route.post('/Parentezcos/Buscar',  BuscarParentezcoPorTexto);
+route.post('/Parentezcos', ValidateCreateParentezco, PostParentezco);
+route.delete('/Parentezcos/:id', DeleteParentezco);
+route.put('/Parentezcos/:id', ValidateUpdateParentezco, PutParentezco);
 
 
 export default route;
